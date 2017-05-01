50b66daa5013fb8a9485974c130bf61f910730c2
javievazq <javievazq@knights.ucf.edu>
2017-04-30 20:04:05 -0400

    finalfinal

--- js/init.js
+++ js/init.js
@@ -1,1 +1,12 @@
-new WOW().init();
+         $(document).ready(function() {
+                   $('#myContainer').multiscroll({
+                   	sectionsColor: ['#fafafa', '#B30838','#B30838','#B30838','#B30838','#633492', '#633492', '#633492', '#633492'],
+                   	anchors: ['home', 'usl', 'teneleven', 'twelvethirteen', 'fourteen', 'mls', 'fifteen', 'sixteen', 'seventeen'],
+                   	menu: '#menu',
+                   	easing: 'easeOutBack',
+         		navigation: true,
+                   	loopBottom: true,
+                   	loopTop: true
+                   });
+               });
+               new WOW().init();
